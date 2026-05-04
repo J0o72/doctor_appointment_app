7a265b23f672496798ced81479539c367e9bbc33
Youssef <youssefabdallah2016@gmail.com>
2026-05-04 15:48:55 +0300

    - create doctor image and text - add new text style

--- lib/core/theming/text_styles.dart
+++ lib/core/theming/text_styles.dart
@@ -1,9 +1,17 @@
+import 'package:doc_appointment_app/core/theming/colors_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
-  static const TextStyle font24Black700Weight = TextStyle(
+  static TextStyle font24Black700Weight = TextStyle(
     color: Colors.black,
-    fontSize: 24,
+    fontSize: 24.sp,
     fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle font32BBlueBold = TextStyle(
+    color: ColorsManager.mainBlue,
+    fontSize: 32.sp,
+    fontWeight: FontWeight.bold,
   );
 }
--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,5 +1,7 @@
 import 'package:doc_appointment_app/features/onboarding/ui/widgets/doc_logo_and_name.dart';
+import 'package:doc_appointment_app/features/onboarding/ui/widgets/doctor_image_and_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -7,7 +9,15 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: Column(children: [DocLogoAndName()])),
+      child: Scaffold(
+        body: Column(
+          children: [
+            DocLogoAndName(),
+            SizedBox(height: 20.h),
+            DoctorImageAndText(),
+          ],
+        ),
+      ),
     );
   }
 }
