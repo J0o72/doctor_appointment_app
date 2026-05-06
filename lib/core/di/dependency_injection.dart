f5fe1ddaa82a6eaa8b22e6411e7469dfd4403e2a
Youssef <youssefabdallah2016@gmail.com>
2026-05-06 18:40:18 +0300

    - create DI file & call it in main

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:doc_appointment_app/core/networking/api_service.dart';
+import 'package:doc_appointment_app/core/networking/dio_factory.dart';
+import 'package:doc_appointment_app/features/login/data/repos/login_repo.dart';
+import 'package:doc_appointment_app/features/login/logic/login_cubit/login_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setUpGetIt() async {
+  // Dio & ApiService
+  Dio dio = DioFactory.getDio();
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  // Login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<ApiService>()));
+  getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()));
+}
