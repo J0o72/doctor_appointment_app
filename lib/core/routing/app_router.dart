a2c893b6e4c3c3372f035930b99cb731539da0e4
Youssef <youssefabdallah2016@gmail.com>
2026-05-06 18:43:53 +0300

    - providing login cubit in app router of LoginScreen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,10 @@
+import 'package:doc_appointment_app/core/di/dependency_injection.dart';
 import 'package:doc_appointment_app/core/routing/routes.dart';
+import 'package:doc_appointment_app/features/login/logic/login_cubit/login_cubit.dart';
 import 'package:doc_appointment_app/features/login/ui/login_screen.dart';
 import 'package:doc_appointment_app/features/onboarding/ui/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
@@ -12,7 +15,12 @@ class AppRouter {
         );
 
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (context) => LoginScreen());
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: LoginScreen(),
+          ),
+        );
 
       default:
         return MaterialPageRoute(
