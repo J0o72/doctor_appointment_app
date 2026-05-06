f776806d62aaa34157e1d26041509ba40289c79c
Youssef <youssefabdallah2016@gmail.com>
2026-05-06 17:29:00 +0300

    - create api result - create dio factory and apply singleton pattern

---
+++ lib/core/networking/dio_factory.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  // private constructor to prevent instantiation from outside the class
+  DioFactory._();
+
+  static Dio? dio;
+
+  // method to get the dio instance, if it is null, create a new one and return it, otherwise return the existing one (singleton pattern)
+  static Dio getDio() {
+    Duration timeout = const Duration(seconds: 30);
+
+    if (dio == null) {
+      dio = Dio();
+      dio!.options.connectTimeout = timeout;
+      dio!.options.receiveTimeout = timeout;
+
+      addDioInterceptors();
+
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  // method to show me in console the request and response of the api calls using the pretty_dio_logger package
+  static void addDioInterceptors() {
+    dio?.interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        requestHeader: true,
+        responseHeader: true,
+      ),
+    );
+  }
+}
