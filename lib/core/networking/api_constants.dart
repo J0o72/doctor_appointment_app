f0d87bd984b47df8bdb3f7b712da31ceba9f4bcd
Youssef <youssefabdallah2016@gmail.com>
2026-05-06 17:58:05 +0300

    - create api service that calls apiEndPoints and login is done

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String baseUrl = 'https://vcare.integration25.com/api/';
-  static const String loginEndpoint = 'auth/login';
+  static const String login = 'auth/login';
 }
