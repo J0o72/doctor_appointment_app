c902654fd3b29cc1bfcce9bf826a2d4c3f361a10
Youssef <youssefabdallah2016@gmail.com>
2026-05-08 01:13:06 +0300

    - create register repo that return success or failure based on coming response from api

---
+++ lib/features/register/data/repos/register_repo.dart
@@ -0,0 +1,22 @@
+import 'package:doc_appointment_app/core/networking/api_error_handler.dart';
+import 'package:doc_appointment_app/core/networking/api_result.dart';
+import 'package:doc_appointment_app/core/networking/api_service.dart';
+import 'package:doc_appointment_app/features/register/data/models/register_request_body.dart';
+import 'package:doc_appointment_app/features/register/data/models/register_response.dart';
+
+class RegisterRepo {
+  final ApiService _apiService;
+
+  RegisterRepo(this._apiService);
+
+  Future<ApiResult<RegisterResponse>> register(
+    RegisterRequestBody registerRequestBody,
+  ) async {
+    try {
+      final response = await _apiService.register(registerRequestBody);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
