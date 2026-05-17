5ff8a53b4660262acb2624b8cb361078729c0149
Youssef <youssefabdallah2016@gmail.com>
2026-05-17 21:29:40 +0300

    test

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -4,6 +4,8 @@ import 'package:doc_appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+// test comment
+
 void main() async {
   setUpGetIt();
   await ScreenUtil.ensureScreenSize();
